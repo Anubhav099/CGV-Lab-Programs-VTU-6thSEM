80ea0479d4ae11746e7972c7f141a670369e059c
Anubhav099 <ait.anubhav@gmail.com>
2023-07-07 12:51:16 +0530

    removed unnecessary line in main function

--- pg1.c
+++ pg1.c
@@ -88,7 +88,6 @@ void main(int argc, char **argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
-    glutInitWindowPosition(0,0);
     glutCreateWindow("Bresenham's Line Drawing");
     
     myInit();
