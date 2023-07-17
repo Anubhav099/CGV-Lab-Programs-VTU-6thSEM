7f057c851d42c8d283bb4dad92881e7bcd23921a
Anubhav099 <ait.anubhav@gmail.com>
2023-07-17 15:53:29 +0530

    MODIFIED Scan Line algorithm

--- pg9.c
+++ pg9.c
@@ -1,47 +1,48 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
-int value;
+int value=0;
 
-float x1 = 200.0, y1 = 200.0, x2 = 100.0, y2 = 300.0, x3 = 200.0, y3 = 400.0, x4 = 300.0, y4 = 300.0;
+int x1=200, y1=200, x2=100, y2=300, x3=200, y3=400, x4=300, y4=300;
 
 void draw_pixel(int x, int y)
 {
-    if (value == 2) glColor3f(0, 0, 1);
-    else            glColor3f(1, 1, 1);
+    if (value == 2) glColor3f(0, 1, 0); //green filling
+    else            glColor3f(1, 1, 1); //white filling
     glBegin(GL_POINTS);
         glVertex2i(x, y);
     glEnd();
     glFlush();
 }
 
-void edgedetect(float x1, float y1, float x2, float y2, int *le, int *re)
+void edgedetect(int x1, int y1, int x2, int y2, int *le, int *re)
 {
-    float mx;
-    if ( y1>y2 ) // y1>y2 ie. y1=400 ,y2=300
+    int mx;
+    if( y1>y2 ) // y1>y2 ie. y1=400 ,y2=300
     {
         int temp = y1;
-        y1 = y2;
+        y1 = y2;            //swap y1,y2
         y2 = temp;
+
         temp = x1;
-        x1 = x2;
+        x1 = x2;            //swap x1,x2
         x2 = temp;
     }
-    if ( y1 != y2) // not a horizontal line
+    if( y1!=y2) // not a horizontal line
         mx = (x2-x1)/(y2-y1);
     else
         mx = x2-x1;
+
     int x = x1;
-    for (int i=y1; i<=y2; i++)
+    for(int i=y1; i<=y2; i++)
     {
-        if (x < (float)le[i])
-            le[i] = (int)x;
-        if (x > (float)re[i])
-            re[i] = (int)x;
+        if(x < le[i])    le[i] = x;
+        if(x > re[i])    re[i] = x;
         x += mx;
     }
 }
-void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4)
+
+void scanfill(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
 {
     int le[500], re[500];
     for (int i = 0; i < 500; i++)
@@ -55,18 +56,18 @@ void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float 
     edgedetect(x3, y3, x4, y4, le, re);
     edgedetect(x4, y4, x1, y1, le, re);
     
-    if (value == 2)
+    if (value == 2) //Fill
     {
-        for (int y = 0; y < 500; y++)
-            if (le[y] <= re[y])
-                for (int i=(int)le[y]+1; i<(int)re[y]; i++)
+        for(int y=0; y<500; y++)
+            if(le[y] <= re[y])
+                for(int i=le[y]+1; i<re[y]-1; i++)
                     draw_pixel(i, y);
     }
-    else
+    else            //Reverese fill
     {
-        for (int y = 499; y >= 0; y--)
-            if (le[y] <= re[y])
-                for (int i=(int)re[y]-1; i>(int)le[y]; i--)
+        for(int y=499; y>=0; y--)
+            if(le[y] <= re[y])
+                for(int i=re[y]; i>le[y]; i--)
                     draw_pixel(i, y);
     }    
 }
@@ -77,7 +78,8 @@ void menu(int num)
     {
     case 0:     exit(0); break;
     case 1:     glClear(GL_COLOR_BUFFER_BIT); break;
-    default:    value = num;
+    case 2:
+    case 3:     value = num;
     }
     glutPostRedisplay();
 }
@@ -96,19 +98,16 @@ void createMenu(void)
 }
 
 void display(void)
-{
-    switch (value)
+{    
+    if(value)
     {
-    case 2:
-        glClear(GL_COLOR_BUFFER_BIT);
-        glColor3f(0.0, 0.0, 0.0);
+        glColor3f(1,1,1);
         glBegin(GL_LINE_LOOP);
             glVertex2f(x1, y1);
             glVertex2f(x2, y2);
             glVertex2f(x3, y3);
             glVertex2f(x4, y4);
-        glEnd();    //NOTE: no BREAK!
-    case 3:
+        glEnd();
         scanfill(x1, y1, x2, y2, x3, y3, x4, y4);
         value = 0;
         glFlush();
@@ -117,13 +116,8 @@ void display(void)
 
 void myinit()
 {
-
-    glClearColor(1, 1, 1, 1);
-    glColor3f(1, 0, 0);
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    
     gluOrtho2D(0, 500, 0, 500);
 }
 
@@ -131,8 +125,7 @@ int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowSize(250, 250);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(500, 500);
     glutCreateWindow("Menu driven Program for Scan filling ");
 
     createMenu();
